58f57ed30734a5de431a57fe886e562b5ff5cedc
Dhruv Dingari <dingari.d@northeastern.edu>
2026-02-18 19:45:12 -0500

    rust http server

---
+++ http-server/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
+const PORT: &str = "7878";
+
+fn main() {
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    // println!(
+    //     "Request: {}",
+    //     String::from_utf8_lossy(&buffer[..])
+    // )
+
+    let contents = fs::read_to_string("../../dhruvdingari8.github.io/index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
